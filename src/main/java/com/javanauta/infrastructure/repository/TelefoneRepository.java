6256bef474f7e5a30631b0dd934ffb58892cfa0f
Natan <natancintra28@gmail.com>
2026-06-18 10:32:18 -0300

    adicionada as classes de security

---
+++ src/main/java/com/javanauta/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.javanauta.infrastructure.repository;
+
+import com.firstproject.aprendendo_spring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
